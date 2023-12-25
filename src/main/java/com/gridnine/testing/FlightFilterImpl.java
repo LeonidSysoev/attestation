3ecedb472be1b1f59cfd6a59f351cb28c9c97d59
LeonidHome <defenser2205@gmail.com>
2023-12-25 23:38:26 +0300

    четвертый коммит

--- src/main/java/com/gridnine/testing/FlightFilterImpl.java
+++ src/main/java/com/gridnine/testing/FlightFilterImpl.java
@@ -7,11 +7,13 @@ import java.util.stream.Collectors;
 
 public class FlightFilterImpl implements FlightFilter {
     private List<Flight> currentFlightList = new ArrayList<>();
-    private List<Segment> segments = new ArrayList<>();
 
     private final LocalDateTime time = LocalDateTime.now();
 
-    @Override
+    /**
+     * Метод выводит список всех перелетов
+     * @param flightList список перелетов
+     */
     public void segmentsOfFlight(List<Flight> flightList) {
         for (Flight flight : flightList) {
             for (int i = 0; i < flight.getSegments().size(); i++) {
@@ -19,58 +21,43 @@ public class FlightFilterImpl implements FlightFilter {
             }
         }
     }
-
+    /**
+     * Метод выводит список всех перелетов где вылет до текущего момента времени
+     * @param flightList список перелетов
+     */
     @Override
     public List<Flight> departureBeforeTheCurrentTime(List<Flight> flightList) {
         currentFlightList = flightList.stream().filter(flight -> flight.getSegments()
                 .stream().anyMatch(segment -> segment.getDepartureDate()
-                        .isBefore(LocalDateTime.now()))).collect(Collectors.toList());
+                        .isBefore(time))).collect(Collectors.toList());
         currentFlightList.forEach(System.out::println);
-//        for (Flight flight : flightList) {
-//            segments.addAll(flight.getSegments());
-////            if (segments.size() <= 0) {
-////                throw new IllegalArgumentException();
-////            }
-//            LocalDateTime departure = segments.get(0).getDepartureDate();
-//            if (departure.isBefore(time)) {
-//                //System.out.println(flight);
-//                currentFlightList.add(flight);
-//                currentFlightList.forEach(System.out::println);
-//            }
-//        }
+
         return currentFlightList;
     }
-
+    /**
+     * Метод выводит список всех перелетов где сегменты с датой прилета раньше даты вылета
+     * @param flightList список перелетов
+     */
     @Override
     public List<Flight> arrivalDateBeforeDepartureDate(List<Flight> flightList) {
-//        for (Flight flight : flightList) {
-//            segments.addAll(flight.getSegments());
-//            if (segments.size() <= 0) {
-//                throw new IllegalArgumentException();
-//            }
-//            LocalDateTime departure = segments.get(0).getDepartureDate();
-//            LocalDateTime arrival = segments.get(0).getArrivalDate();
-//            if (arrival.isBefore(departure)) {
-//                System.out.println(flight);
-//                currentFlightList.add(flight);
-//            }
-//        }
         currentFlightList = flightList.stream().filter(flight -> flight.getSegments()
                 .stream().anyMatch(segment -> segment.getArrivalDate()
                         .isBefore(segment.getDepartureDate()))).collect(Collectors.toList());
         currentFlightList.forEach(System.out::println);
         return currentFlightList;
     }
-
+    /**
+     * Метод выводит список всех перелетов где время проведенное на земле превышает 2 часа
+     * @param flightList список перелетов
+     */
     @Override
     public List<Flight> theTotalTimeSpentOnTheGroundExceedsTwoHours(List<Flight> flightList) {
         for (Flight flight : flightList) {
-            //segments.addAll(flight.getSegments());
-            segments = flight.getSegments();
+            List<Segment> segments = flight.getSegments();
             for (int i = 0; i < segments.size() - 1; i++) {
                 LocalDateTime departure = segments.get(i + 1).getDepartureDate();
                 LocalDateTime arrival = segments.get(i).getArrivalDate();
-                if (arrival.plusHours(2).isBefore(departure)) {
+                if (departure.isAfter(arrival.plusHours(2))) {
                     currentFlightList.add(flight);
                 }
             }
